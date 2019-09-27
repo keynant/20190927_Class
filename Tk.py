ba2731d12056719cce1d25ec7c4263cff1cc98cf
keynant <52064993+keynant@users.noreply.github.com>
2019-09-27 12:22:51 +0300

    Update Tk.py

--- Tk.py
+++ Tk.py
@@ -13,12 +13,12 @@ class CalculatorWindow:
         self.addBtn1 = Button(text="Add",
                              command=lambda: self.total_value.set(self.total_value.get() + 1))
         self.addBtn1.pack()
-        self.addBtn2 = Button(text="Subtract",
-                              command=lambda: self.total_value.set(self.total_value.get() - 1))
-        self.addBtn2.pack()
-        self.addBtn3 = Button(text="Reset",
-                              command=lambda: self.total_value.set(0))
-        self.addBtn3.pack()
+        self.subBtn = Button(text="Subtract",
+                             command=lambda: self.total_value.set(self.total_value.get() - 1))
+        self.subBtn.pack()
+        self.rstBtn = Button(text="Reset",
+                             command=lambda: self.total_value.set(0))
+        self.rstBtn.pack()
 
         self.dateLabel = StringVar()
         self.dateLabel.set("Hello")
@@ -26,9 +26,9 @@ class CalculatorWindow:
                                  textvariable=self.dateLabel,
                                  font=("david", 72))
         self.total_label2.pack()
-        self.addBtn4 = Button(text="Show Date",
-                              command=lambda: self.dateLabel.set(time.ctime()))
-        self.addBtn4.pack()
+        self.updateTimeBtn = Button(text="Show Date",
+                                    command=lambda: self.dateLabel.set(time.ctime()))
+        self.updateTimeBtn.pack()
 
 
 
